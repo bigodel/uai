af56e92d0351c7e2553bae41766ada17ce2ea6b5
João Pedro <jpedrodeamorim@gmail.com>
2022-11-24 13:01:23 -0300

    formatacao

--- trem.h
+++ trem.h
@@ -6,26 +6,27 @@
 /*
  * Classe Trem herda QThread
  * Classe Trem passa a ser uma thread.
- * A função START inicializa a thread. Após inicializada, a thread irá executar a função RUN.
- * Para parar a execução da função RUN da thread, basta executar a função TERMINATE.
+ * A função START inicializa a thread. Após inicializada, a thread irá executar
+ * a função RUN. Para parar a execução da função RUN da thread, basta executar a
+ * função TERMINATE.
  *
-*/
-class Trem: public QThread{
- Q_OBJECT
+ */
+class Trem : public QThread {
+  Q_OBJECT
 public:
-    Trem(int,int,int);  //construtor
-    void run();         //função a ser executada pela thread
+  Trem(int, int, int); // construtor
+  void run();          // função a ser executada pela thread
 
-
-//Cria um sinal
+  // Cria um sinal
 signals:
-    void updateGUI(int,int,int);
+  void updateGUI(int, int, int);
 
 private:
-   int x;           //posição X do trem na tela
-   int y;           //posição Y do trem na tela
-   int ID;          //ID do trem
-   int velocidade;  //Velocidade. É o tempo de dormir em milisegundos entre a mudança de posição do trem
+  int x;          // posição X do trem na tela
+  int y;          // posição Y do trem na tela
+  int ID;         // ID do trem
+  int velocidade; // Velocidade. É o tempo de dormir em milisegundos entre a
+                  // mudança de posição do trem
 };
 
 #endif // TREM_H
